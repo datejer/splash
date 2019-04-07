3922e580eba3681f0970f0163dff1cd8ac3f0c98
ejer <35652893+datejer@users.noreply.github.com>
2019-04-07 18:05:41 +0200

    Update config.

---
+++ config.js
@@ -0,0 +1,8 @@
+exports.content = {
+    token: token,
+    prefix: "u!",
+    activityMessage: "beautiful photos. | u!help",
+    activityType: "WATCHING",
+    unsplashAccessKey: accessKey,
+    unsplashSecretKey: secretKey
+};
